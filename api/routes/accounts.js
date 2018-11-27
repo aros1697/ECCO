e39cae4bbef05b532345857d0f07c8554b063cfe
Anton Brandt Svärd <svan1600@student.ju.se>
2018-11-27 16:23:06 +0100

    login auth

--- api/routes/accounts.js
+++ api/routes/accounts.js
@@ -4,6 +4,7 @@ const multer = require('multer')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const checkAuth = require('../middleware/check-auth')
+const connection = require('../../db')
 
 const jwtSecret = "jknasbdhbsadvsavdagsdsdvraw"
 
@@ -55,36 +56,74 @@ router.post('/signup', upload.single('avatar'), (request, response, next) => {
             email: request.body.email,
             avatar: request.file.path
             }
-            response.status(201).json({
-                message: "Account was created",
-                createdUser: user
+            const query = "INSERT INTO accounts (username, password, email, avatar) VALUES (?, ?, ?, ?)"
+            const values = [user.username, user.password, user.email, user.avatar]
+        
+            connection.query(query, values, function(error, result){
+                if (error){
+                    if(error.errno == 1062){
+                        response.status(400).json({error: "Username not Unique"})
+                    } else {
+                        response.status(500).json({
+                            message: error
+                        }).end()
+                    }
+                } else {
+                    const username = result.insertUsername
+                    
+                    response.setHeader("Location", "/accounts/"+username)
+                    response.status(201).json({
+                        message: "User created",
+                        user: user
+                    }).end()
+                }
             })
         }
     })
 })
 
 router.post('/login', (request, response, next) => {
-    bcrypt.compare(request.body.password, user[0].password, (err, result) => {
+    const user = {
+        username: request.body.username,
+        password: request.body.password
+        }
+
+    bcrypt.compare(user.password, user[0].password, (err, result) => {
         if (err) {
             response.status(401).json({
                 message: "Authorization failed"
             })
-        } 
-        if (result) {
-            const accessToken = jwt.sign({
-                username: user[0].username,
-            }, jwtSecret,
-            {
-                expiresIn: "1h"
-            }
-            )
-            response.status(200).json({
-                message: "Authorization successful",
-                accessToken: accessToken
-            })
         }
-        response.status(401).json({
-            message: "Authorization failed"
+
+        const query = "SELECT * FROM accounts WHERE username = ?"
+        const values = [username]
+        
+        connection.query(query, values, function(error, users){
+            if (error) {
+                response.status(500).end()
+            } else {
+                if(users.length == 0){
+                    response.status(400).json({error: "invalid_grant"})
+                    return
+                }
+                const user = user[0]
+                if (user.password === password){
+                    const accessToken = jwt.sign({
+                        username: user.username,
+                    }, jwtSecret,
+                    {
+                        expiresIn: "1h"
+                    }
+                    )
+                    response.status(200).json({
+                        message: "Authorization successful",
+                        accessToken: accessToken
+                    })
+                }
+                response.status(401).json({
+                    message: "Authorization failed"
+                })
+            }
         })
     })
 })
--- db.js
+++ db.js
@@ -7,32 +7,39 @@ const connection = mysql.createConnection({
     password: "Tksgisopdi7t"
 })
 
+connection.connect(function(err)
+{
+    if(!err){
+		console.log("DB connection succeded");
+	} else
+    console.log("DB connection failed \n Error :" + JSON.stringify(err,undefined,2));
+})
+
 connection.query(`CREATE TABLE IF NOT EXISTS accounts (
     username VARCHAR(50) UNIQUE PRIMARY KEY,
     password VARCHAR(100),
-    email VARCHAR(100)
-    avatar 
+    email VARCHAR(100),
+    avatar VARCHAR(100)
 )`)
 
 connection.query(`CREATE TABLE IF NOT EXISTS posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     contentPost VARCHAR(100),
-    accountId INT,
+    username VARCHAR(50),
     FOREIGN KEY (username) REFERENCES accounts(username) ON DELETE CASCADE  
 )`)
 
 connection.query(`CREATE TABLE IF NOT EXISTS comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     contentComments VARCHAR(100),
-    accountId INT,    
+    username VARCHAR(50),    
     FOREIGN KEY (username) REFERENCES accounts(username) ON DELETE CASCADE
-    
 )`)
 
 connection.query(`CREATE TABLE IF NOT EXISTS privateMessage (
     id INT AUTO_INCREMENT PRIMARY KEY,
     contentPM VARCHAR(100),
-    accountId INT,
+    username VARCHAR(50),
     FOREIGN KEY (username) REFERENCES accounts(username) ON DELETE CASCADE    
 )`)
 
